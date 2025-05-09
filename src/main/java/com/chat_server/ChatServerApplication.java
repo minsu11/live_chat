896a681e0f06534d8a23f81201d0407782b58ebc
minsu11 <zmfjtnl1@naver.com>
2025-05-09 19:52:13 +0900

    [Feat] feign clients dependency 추가

--- src/main/java/com/chat_server/ChatServerApplication.java
+++ src/main/java/com/chat_server/ChatServerApplication.java
@@ -3,9 +3,11 @@ package com.chat_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class ChatServerApplication {
 
     public static void main(String[] args) {
