4160bed422877a48b1a6612d35385616d7145778
minsu11 <zmfjtnl1@naver.com>
2025-05-22 17:57:17 +0900

    [Feat] exception 추가

---
+++ src/main/java/com/chat_server/util/CookieUtil.java
@@ -0,0 +1,14 @@
+package com.chat_server.util;
+
+/**
+ *packageName    : com.chat_server.util
+ * fileName       : CookieUtil
+ * author         : parkminsu
+ * date           : 25. 5. 22.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 5. 22.        parkminsu       최초 생성
+ */public class CookieUtil {
+}
