6af272d5b9bd3b41b2b69ad2f54456b10bcf2840
minsu11 <zmfjtnl1@naver.com>
2025-05-28 17:49:02 +0900

    [Feat] access token 재발급 로직 변경으로 인해 try catch부분 수정

--- src/main/java/com/chat_server/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/chat_server/common/filter/JwtAuthenticationFilter.java
@@ -5,12 +5,14 @@ import com.chat_server.authentication.verifier.JwtVerifier;
 import com.chat_server.user.service.UserService;
 import com.chat_server.util.CookieUtil;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -29,6 +31,7 @@ import java.io.IOException;
  * -----------------------------------------------------------
  * 25. 5. 21.        parkminsu       최초 생성
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -37,13 +40,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final UserService userService;
 
-
-    // redis service
     // jwt token 검증
     // 토큰에 대한 기본적인 검증
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // token 
+        // token
         String accessToken = CookieUtil.getCookie(request, "access_token");
 
         // token data null
@@ -56,16 +57,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             // principal 가지고옴
             UserPrincipal userPrincipal = userService.loadUserByUserId(claims.getId());
 
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userPrincipal, null, null);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
 
             // context holder에 저장
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
+
+        } catch (ExpiredJwtException expiredJwtException) {
+            // 만료된 토큰은 front서버에서 응답
+            log.debug("Expired JWT token");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Token Expired");
+            SecurityContextHolder.clearContext();
         } catch (JwtException ignored) {
             // 여기서 token 검증
-            
-
+            log.error("Invalid JWT token");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 토큰 들어옴");
+            SecurityContextHolder.clearContext();
         }
+
+        filterChain.doFilter(request, response);
+
 
     }
 }
