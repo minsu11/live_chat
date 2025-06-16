e219de27fc84f3ff8e7885fa2e108c5d8369cf56
minsu <zmfjtnl1@naver.com>
2025-06-16 22:31:40 +0900

    [Feat] 작업

--- src/main/java/com/chat_server/common/config/CommonConfig.java
+++ src/main/java/com/chat_server/common/config/CommonConfig.java
@@ -2,6 +2,7 @@ package com.chat_server.common.config;
 
 import com.chat_server.common.redis.config.RedisConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.resource.DefaultClientResources;
 import jakarta.servlet.ServletContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.RegistrationBean;
@@ -9,12 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 /**
  * packageName    : com.chat_server.common.config
@@ -31,6 +35,9 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class CommonConfig {
     private final RedisConfig redisConfig;
+    // 해당 설정들은 명시적으로 리소스 관리를 해주기 위한 것,
+    // 애플리케이션 종료 시 Shutdown
+    private final DefaultClientResources clientResources = DefaultClientResources.create();
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -39,15 +46,21 @@ public class CommonConfig {
 
 
     // todo reids time connect 등 여러 설정들을 만져야함
-
+    // redis 비밀번호 설정도 추가해야함
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(redisConfig.getHost());
         config.setPort(redisConfig.getPort());
 
-        RedisConnectionFactory factory = new LettuceConnectionFactory(config);
-        return factory;
+        // time out 5초
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(5))
+                .shutdownTimeout(Duration.ofMillis(100))
+                .clientResources(clientResources)
+                .build();
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
