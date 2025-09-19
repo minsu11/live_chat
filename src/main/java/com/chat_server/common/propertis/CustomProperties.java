a894ec93cc3210e656b6b3bb4ad00555a6d48000
민수 박 <zmfjtnl1@naver.com>
2025-09-19 13:26:03 +0900

    [Fix] error code NullPointException 해결중

--- src/main/java/com/chat_server/common/propertis/CustomProperties.java
+++ src/main/java/com/chat_server/common/propertis/CustomProperties.java
@@ -3,6 +3,7 @@ package com.chat_server.common.propertis;
 import com.chat_server.error.enumulation.ErrorCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,12 +33,14 @@ public class CustomProperties {
 
     @Getter
     @Setter
+    @Slf4j
     public static class Error {
         private Map<String, String> messages;
 
         public String getMessages(ErrorCode errorCode){
+            log.error(errorCode.toString());
             if(messages == null){
-                return messages.get(ErrorCode.NOT_DEFINE.name());
+                return "error code 없음";
             }
             return messages.getOrDefault(errorCode.name(), "알 수 없는 에러 입니다.");
         }
