5a297e7c7501ff8ee562821a21cb5fb82fc6e24a
minsu <zmfjtnl1@naver.com>
2025-10-02 00:04:45 +0900

    [Refact] @RequestParm 아닌 @PathVariable 변경

--- src/main/java/com/chat_server/userprofile/controller/UserProfileController.java
+++ src/main/java/com/chat_server/userprofile/controller/UserProfileController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -68,7 +65,7 @@ public class UserProfileController {
      */
     @GetMapping("{userId}/profile/detail")
     public ResponseEntity<ApiResponse<UserMyProfileDetailResponse>> getMyProfileDetail(
-        @RequestParam(name = "userId") String userId
+            @PathVariable(name = "userId")  String userId
     ){
         log.info("getMyProfileDetail");
         log.info("userId: " + userId);
