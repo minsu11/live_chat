1fa49d362633424649ebbaa352042cc1c812baf0
민수 박 <zmfjtnl1@naver.com>
2025-10-07 16:58:57 +0900

    [Feat] User profile url service 구조 생성

---
+++ src/main/java/com/chat_server/userprofile/url/service/UserProfileUrlService.java
@@ -0,0 +1,5 @@
+package com.chat_server.userprofile.url.service;
+
+public interface UserProfileUrlService {
+
+}
---
+++ src/main/java/com/chat_server/userprofile/url/service/impl/UserProfileUrlServiceImpl.java
@@ -0,0 +1,7 @@
+package com.chat_server.userprofile.url.service.impl;
+
+import com.chat_server.userprofile.url.service.UserProfileUrlService;
+
+public class UserProfileUrlServiceImpl implements UserProfileUrlService {
+
+}
