e3f01c5db7921e7cf137cb9d70653c73bba7ba32
minsu <zmfjtnl1@naver.com>
2025-11-04 17:03:32 +0900

    [Fix] 레포지토리와 response field 위치 조정

--- src/main/java/com/chat_server/chatlist/dto/response/ChatRoomListResponse.java
+++ src/main/java/com/chat_server/chatlist/dto/response/ChatRoomListResponse.java
@@ -3,10 +3,9 @@ package com.chat_server.chatlist.dto.response;
 import java.time.LocalDateTime;
 
 public record ChatRoomListResponse (
-    String chatRoomName,
-    LocalDateTime lastMessageAt,
-    long chatRoomId
-
+        Long chatroomId,
+        String chatRoomName,
+        LocalDateTime lastMessageAt
 ){
 
 }
