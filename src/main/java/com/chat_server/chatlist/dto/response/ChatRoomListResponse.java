69e62409561a142519def0cc3adec4aa6338c05c
minsu <zmfjtnl1@naver.com>
2025-11-04 17:07:26 +0900

    [Refact] response field 이름 변경

--- src/main/java/com/chat_server/chatlist/dto/response/ChatRoomListResponse.java
+++ src/main/java/com/chat_server/chatlist/dto/response/ChatRoomListResponse.java
@@ -3,7 +3,7 @@ package com.chat_server.chatlist.dto.response;
 import java.time.LocalDateTime;
 
 public record ChatRoomListResponse (
-        Long chatroomId,
+        Long chatRoomId,
         String chatRoomName,
         LocalDateTime lastMessageAt
 ){
