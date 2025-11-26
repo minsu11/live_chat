0d80a45099ca7db8cfbd77238fd42dd7730486a1
minsu11 <zmfjtnl1@naver.com>
2025-11-26 14:07:30 +0900

    [Refact] dml 수정

--- src/main/resources/script/dml.sql
+++ src/main/resources/script/dml.sql
@@ -1,9 +1,9 @@
-insert into `user_status`(`user_status_name`) values ('활성');
-insert into `user_status`(`user_status_name`) values('비활성화');
-insert into `user_status`(`user_status_name`) values('휴먼');
+insert into `user_status`(`name`) values ('활성');
+insert into `user_status`(`name`) values('비활성화');
+insert into `user_status`(`name`) values('휴먼');
 
-insert into `gender` (`gender_name`) values('남자');
-insert into `gender` (`gender_name`) values('여자');
+insert into `gender` (`name`) values('남자');
+insert into `gender` (`name`) values('여자');
 
 
 -- 임시 데이터
