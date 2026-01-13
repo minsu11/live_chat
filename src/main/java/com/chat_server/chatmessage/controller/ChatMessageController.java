caf217cc509437b28eebc80d997191f70833afa1
민수 박 <zmfjtnl1@naver.com>
2026-01-13 14:54:32 +0900

    [Fix] json 인자 에러 해결

--- src/main/java/com/chat_server/chatmessage/controller/ChatMessageController.java
+++ src/main/java/com/chat_server/chatmessage/controller/ChatMessageController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,15 @@ public class ChatMessageController {
     @MessageMapping("chat/message")
     public void sendMessage(
                         ChatSendRequest chatSendRequest,
-                        @AuthenticationPrincipal AuthenticatedUser authenticatedUser
+                        Authentication authentication
                         ) {
         log.info("chat message controller start");
-        log.info("user id: {}", authenticatedUser.userId());
-        Long user = authenticatedUser.userId();
-
+//        log.info("user id: {}", authenticatedUser.userId());
+//        Long user = authenticatedUser.userId();
+        AuthenticatedUser authenticatedUser = (AuthenticatedUser) authentication.getPrincipal();
+        Long userId= authenticatedUser.userId();
+        log.info("userId: {}",userId);
+        log.info("chat message controller end");
     }
 
 }
